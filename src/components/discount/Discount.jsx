95ce3ab93ec3f3c1ac4e2b011f7d64c1e9c7fd7a
jeliaaa <julia.aleqsandre@gmail.com>
2023-07-25 22:11:41 +0400

    .

--- src/components/discount/Discount.jsx
+++ src/components/discount/Discount.jsx
@@ -13,47 +13,47 @@ const Discount = () => {
   const [data, setData] = useState([]);
   useEffect(() => {
     axios
-      .get("https://api.escuelajs.co/api/v1/products?limit=9&offset=3")
+      .get("https://api.escuelajs.co/api/v1/products?limit=11&offset=3")
       .then((res) => setData(res.data));
   }, []);
-  const slicedData = data.slice(0, 3);
+  const slicedData = data.slice(8, 11);
   return (
     <div className="discount">
       <section className="discount-left">
-        <img src={imgDisc} alt=""/>
+        <img src={imgDisc} alt="" />
       </section>
       <section className="discount-right">
         <div className="div-heading">
-        <h1><span>MACC</span>WEEKLY DISCOUNT</h1>
-        <button>VIEW ALL</button>
+          <h1><span>MACC</span>WEEKLY DISCOUNT</h1>
+          <button>VIEW ALL</button>
         </div>
-      <Swiper
-        className="mySwiper2"
-        slidesPerView={1}
-        spaceBetween={50}
-        keyboard={true}
-        navigation={true}
-        modules={[Navigation, Mousewheel, Keyboard]}
-        breakpoints={{
-          1250:{
-            slidesPerView: 2
-          }
-        }}
-      >
-        {slicedData.map((item) => {
-          return (
-            <SwiperSlide key={item.id}>
-              <Item
-                key={item.id}
-                name={item.title}
-                image={item.images[1]}
-                id={item.id}
-                price={item.price}
-              ></Item>
-            </SwiperSlide>
-          );
-        })}
-      </Swiper>
+        <Swiper
+          className="mySwiper2"
+          slidesPerView={1}
+          spaceBetween={50}
+          keyboard={true}
+          navigation={true}
+          modules={[Navigation, Mousewheel, Keyboard]}
+          breakpoints={{
+            1250: {
+              slidesPerView: 2
+            }
+          }}
+        >
+          {slicedData.map((item) => {
+            return (
+              <SwiperSlide key={item.id}>
+                <Item
+                  key={item.id}
+                  name={item.title}
+                  image={item.images[1]}
+                  id={item.id}
+                  price={item.price}
+                ></Item>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
       </section>
     </div>
   );
